1f17725bbdd9f057c1c76c5f27803f72ceb2971c
sachin54patel <sachin54patel@gmail.com>
2022-04-08 22:59:57 +0530

    sachin

--- modules/user.js
+++ modules/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/pms');
+mongoose.connect('mongodb+srv://root:E7SCKKLbZosIfw4P@cluster0.mhm4k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 var conn = mongoose.Collection;
 var UserSchema = new mongoose.Schema({
     username : {
